66b1768c2547fac25c9ef9304b866738e08d8785
spaceshuttl <nikolashenrijs@gmail.com>
2026-05-16 00:53:25 +0300

    Izlaboju datubazes tabulas un pievienoju vel laukus

--- database_creator.py
+++ database_creator.py
@@ -22,9 +22,10 @@ CREATE TABLE IF NOT EXISTS venues (
     
 CREATE TABLE IF NOT EXISTS events (
     id INTEGER PRIMARY KEY,
-    date DATE,
-    sales_start DATE,
-    sales_end DATE,
+    title VARCHAR(300),
+    date TIMESTAMP,
+    sales_start TIMESTAMP,
+    sales_end TIMESTAMP,
     description VARCHAR(10000),
     language VARCHAR(50),
     venue_id INTEGER,
@@ -40,26 +41,34 @@ CREATE TABLE IF NOT EXISTS tickets (
     checked_on TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY (event_id) REFERENCES events(id) ON DELETE CASCADE
 );
-
 """)
 
-# ievada informaciju tabula
-cur.execute("""
-INSERT INTO venues (id, name, address, longitude, latitude) VALUES (%s, %s, %s, %s, %s)""", (249,'BTA Daugavas Stadions','Rīga, Augšiela 1', 56.953675, 24.157391)
-)
+# testesanai ievada informaciju tabula
+# cur.execute("""
+# INSERT INTO venues (
+#     id, name, address, longitude, latitude) VALUES (%s, %s, %s, %s, %s)""",
+# (249,'BTA Daugavas Stadions','Rīga, Augšiela 1', 56.953675, 24.157391)
+# )
+
+# izdzes visas izveidotas tabulas
+# cur.execute("""
+#             DROP TABLE IF EXISTS venues cascade;
+#             DROP TABLE IF EXISTS events cascade;
+#             DROP TABLE IF EXISTS tickets cascade;
+#             """)
 
 # izpilda izmainas
 conn.commit()
 
 # izvada visas rindas
-cur.execute("""
-SELECT * FROM venues;
-""")
-
-rows = cur.fetchall()
-
-for row in rows:
-    print(row)
+# cur.execute("""
+# SELECT * FROM venues;
+# """)
+#
+# rows = cur.fetchall()
+#
+# for row in rows:
+#     print(row)
 
 # beidz savienojumu
 cur.close()
