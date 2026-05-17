e2878eae9de6b0eff420d3f9c255947b9a43866d
spaceshuttl <nikolashenrijs@gmail.com>
2026-05-18 02:04:25 +0300

    Izmainiju uz tabulas veida srakstu, kur ari var secigi sakartot ierakstus pec vertibam

--- backend/static/script.js
+++ backend/static/script.js
@@ -1,25 +1,56 @@
+// MEKLESANAS FUNKCIJA
+document.addEventListener("DOMContentLoaded", function () {
+
 // Atrod meklersanas elementu pec id
-const searchInput = document.getElementById("searchInput");
+    const searchInput = document.getElementById("searchInput");
 
 // funkcijas, kas veic meklesanu, kad kko ievada meklesana
-searchInput.addEventListener("keyup", function ()
-{
-    // Parveido meklesana ievadito vertibu uz mazajiem burtiem
-    const value = this.value.toLowerCase();
+    searchInput.addEventListener("keyup", function () {
+        // Parveido meklesana ievadito vertibu uz mazajiem burtiem
+        const value = this.value.toLowerCase();
 
-    // Atrod visus cards, kas veido notikumu sarakstu
-    const cards = document.querySelectorAll(".event-card");
+        // Atrod visus cards, kas veido notikumu sarakstu
+        const rows = document.querySelectorAll(".event-row");
 
-    // No katra card iegust taja esoso tekstu un to padara par neredzamu, ja neatbilst ierakstitajam meklesana
-    cards.forEach(card => {
-        const text = card.innerText.toLowerCase();
-        if (text.includes(value))
-        {
-            card.style.display = "";
-        }
-        else
-        {
-            card.style.display = "none";
-        }
+        // No katra card iegust taja esoso tekstu un to padara par neredzamu, ja neatbilst ierakstitajam meklesana
+        rows.forEach(row => {
+            const text = row.innerText.toLowerCase();
+            if (text.includes(value)) {
+                row.style.display = "";
+            } else {
+                row.style.display = "none";
+            }
+        });
     });
 });
+
+
+
+// KOLONNU KARTOSANAS FUNKCIJA
+function sortTable(columnIndex) {
+
+    const table = document.querySelector("table tbody");
+    const rows = Array.from(table.querySelectorAll("tr"));
+    const sortedRows = rows.sort((a, b) => {
+
+        let aText = a.children[columnIndex].innerText.trim();
+        let bText = b.children[columnIndex].innerText.trim();
+
+        // Remove euro symbol and commas
+        aText = aText.replace("€", "").replace(",", "");
+        bText = bText.replace("€", "").replace(",", "");
+
+        // Numeric sorting
+        if (!isNaN(aText) && !isNaN(bText))
+        {
+            return Number(aText) - Number(bText);
+        }
+        // Text sorting
+        return aText.localeCompare(bText);
+
+    });
+
+    // Re-add rows
+    rows.forEach(row => row.remove());
+    sortedRows.forEach(row => {table.appendChild(row);});
+}
